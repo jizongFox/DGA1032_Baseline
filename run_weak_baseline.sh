f7997d45905c5fd8c7cf668ae465e1e68df464b3
jizong <pengjizong@sjtu.edu.cn>
2018-09-03 15:32:34 -0400

    check the weakly supervised setting.

--- run_weak_baseline.sh
+++ run_weak_baseline.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=0  python weakly-main.py --baseline ADMM_weak &
-MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=1  python weakly-main.py --baseline ADMM_weak_gc &
-MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=2  python weakly-main.py --baseline ADMM_weak_size
+MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=1  python weakly-main.py --baseline ADMM_weak &
+MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=2  python weakly-main.py --baseline ADMM_weak_gc &
+MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=3  python weakly-main.py --baseline ADMM_weak_size
 
