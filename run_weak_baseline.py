e7a1a5770194440787c7a3dbf0eda7f632d1e0d7
jizong <pengjizong@sjtu.edu.cn>
2018-09-05 13:48:22 -0400

    check the weakly supervised setting.

--- run_weak_baseline.py
+++ run_weak_baseline.py
@@ -1,12 +1,13 @@
 import os,numpy as np
-epses = [0.05, 0.1, 0.2, 0.4, 0.6]
-baselines = ['ADMM_weak','ADMM_weak_gc','ADMM_weak_size']
+epses = [0.01, 0.05, 0.1, 0.2, 0.4, 0.6]
+baselines = ['ADMM_weak','ADMM_weak_size']
 choice_of_GPU = [0,1]
 cmds = []
 for eps in epses:
     for baseline in baselines:
         cmd = "MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=%d  python weakly-main.py --baseline %s --eps %f"%(np.random.choice(choice_of_GPU),baseline,eps)
         cmds.append(cmd)
+cmds.append("MP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=%d  python weakly-main.py --baseline %s --eps %f"%(np.random.choice(choice_of_GPU),'ADMM_weak_gc',0))
 
 if __name__ =='__main__':
     from multiprocessing import Pool
